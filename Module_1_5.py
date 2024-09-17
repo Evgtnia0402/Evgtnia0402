9e88d89319e0ecb53a0fc0759d2b970e931db181
Evgenia <serogodskaaevgenia@gmail.com>
2024-09-17 20:55:26 +0300

    Create Module_1_5.py

---
+++ Module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var= "a","b",1,2,True,False,[1,2]
+typle=immutable_var
+print(tuple)
+#tuple_[3] = "c" - содержимое кортежных скобок не изменно
+mutable_list = ["a","b",1,2,True,False,[1,2]]
+print(mutable_list)
+mutable_list[3] = "c"
+print(mutable_list)
